453d30a3c474d30069a11d1f3efc243a721d7085
Iago Abal <mail@iagoabal.eu>
2021-04-05 10:45:42 +0000

    WIP: taint tests wrapper functions

---
+++ semgrep-core/tests/tainting_rules/ts/2787-fun.ts
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class BadPractice extends Component {
+  dangerous() {
+    return location.href;
+  }
+
+  render() {
+    // Testing DOM XSS source --> sink taint tracking in `mode: taint`
+
+    //ERROR:
+    React.createElement("a", { href: this.dangerous() });
+  }
+}
+export default BadPractice;
+
