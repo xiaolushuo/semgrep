xiaolushuo/semgrep

commit 70a20605ae403c2f6a223eb779883238862fc0b1

metavariable-pattern: Allow matching content under another language (#3366)
* metavariable-pattern: Allow matching content under another language

test plan:
make test # test included

* match_rules: Move helpers to separate top-level functions

* match_rules: Move metavariable-pattern logic to a separate function

* match_rules: Restrict metavariable-pattern+language to text

semgrep-core/src/engine/Match_rules.ml
@@ -796,48 +796,8 @@ and filter_ranges env xs cond =
          | R.CondGeneric e ->
              let env = Eval_generic.bindings_to_env bindings in
              Eval_generic.eval_bool env e
-         | R.CondPattern (mvar, formula) -> (
-             (* If anything goes wrong the default is to filter out! *)
-             match List.assoc_opt mvar bindings with
-             | None ->
-                 logger#error "rule %s: metavariable-pattern: %s not found"
-                   env.rule.id mvar;
-                 false
-             | Some mval -> (
-                 match MV.program_of_mvalue mval with
-                 | None ->
-                     logger#error
-                       "rule %s: metavariable-pattern: %s does not bound a \
-                        sub-program"
-                       env.rule.id mvar;
-                     false
-                 | Some mast -> (
-                     let mval_range = MV.range_of_mvalue mval in
-                     let lazy_ast_and_errors = lazy (mast, []) in
-                     let lazy_content =
-                       lazy (Range.content_at_range env.file mval_range)
-                     in
-                     (* this following code is very similar to `check' below;
-                      * we could maybe factorize things *)
-                     let xpatterns = xpatterns_in_formula formula in
-                     let res =
-                       matches_of_xpatterns env.config env.rule env.equivalences
-                         (env.file, env.xlang, lazy_ast_and_errors, lazy_content)
-                         xpatterns
-                     in
-                     let pattern_matches_per_id =
-                       group_matches_per_pattern_id res.matches
-                     in
-                     let env =
-                       { env with pattern_matches = pattern_matches_per_id }
-                     in
-                     match
-                       evaluate_formula env
-                         (Some { r with r = mval_range })
-                         formula
-                     with
-                     | [] -> false
-                     | _ :: _ -> true ) ) )
+         | R.CondPattern (mvar, opt_lang, formula) ->
+             match_range_with_pattern env r mvar opt_lang formula
          (* todo: would be nice to have CondRegexp also work on
           * eval'ed bindings.
           * We could also use re.match(), to be close to python, but really
@@ -870,6 +830,82 @@ and filter_ranges env xs cond =
                Eval_generic.bindings_to_env_with_just_strings bindings
              in
              Eval_generic.eval_bool env e)
+
+and match_range_with_pattern env r mvar opt_lang formula =
+  let bindings = r.mvars in
+  (* If anything goes wrong the default is to filter out! *)
+  match List.assoc_opt mvar bindings with
+  | None ->
+      (* THINK: fatal error instead? *)
+      logger#error "rule %s: metavariable-pattern: %s not found" env.rule.id
+        mvar;
+      false
+  | Some mval -> (
+      let mval_range = MV.range_of_mvalue mval in
+      match (opt_lang, mval) with
+      | Some lang, MV.Text (content, _)
+      | Some lang, MV.E (G.L (G.String (content, _))) ->
+          (* The matched text will be interpreted according to `lang'. *)
+          let lazy_ast_and_errors =
+            lazy_ast_of_string lang content (fun () ->
+                spf
+                  "rule %s: metavariable-pattern: failed to fully parse the \
+                   content of %s"
+                  env.rule.id mvar)
+          in
+          eval_nested_formula env env.xlang formula lazy_ast_and_errors
+            (lazy content)
+            None
+      | Some _lang, _mval ->
+          (* THINK: fatal error instead? *)
+          logger#error
+            "rule %s: metavariable-pattern: the content of %s is not text"
+            env.rule.id mvar;
+          false
+      | None, _mval -> (
+          match MV.program_of_mvalue mval with
+          | None ->
+              (* THINK: fatal error instead? *)
+              logger#error
+                "rule %s: metavariable-pattern: %s does not bound a sub-program"
+                env.rule.id mvar;
+              false
+          | Some mast ->
+              let lazy_ast_and_errors = lazy (mast, []) in
+              let lazy_content =
+                lazy (Range.content_at_range env.file mval_range)
+              in
+              eval_nested_formula env env.xlang formula lazy_ast_and_errors
+                lazy_content
+                (Some { r with r = mval_range }) ) )
+
+and lazy_ast_of_string lang str warn_msg =
+  lazy
+    (let ext =
+       match Lang.ext_of_lang lang with x :: _ -> x | [] -> assert false
+     in
+     Common2.with_tmp_file ~str ~ext (fun file ->
+         let { Parse_target.ast; errors; _ } =
+           Parse_target.parse_and_resolve_name_use_pfff_or_treesitter lang file
+         in
+         if errors <> [] then pr2 (warn_msg ());
+         (ast, errors)))
+
+and eval_nested_formula env xlang formula lazy_ast_and_errors lazy_content
+    opt_context =
+  (* the following code is very similar to `check' below;
+     * we could maybe factorize things *)
+  let xpatterns = xpatterns_in_formula formula in
+  let res =
+    matches_of_xpatterns env.config env.rule env.equivalences
+      (env.file, xlang, lazy_ast_and_errors, lazy_content)
+      xpatterns
+  in
+  let pattern_matches_per_id = group_matches_per_pattern_id res.matches in
+  let env = { env with pattern_matches = pattern_matches_per_id } in
+  match evaluate_formula env opt_context formula with
+  | [] -> false
+  | _ :: _ -> true
   [@@profiling]
 
 (*****************************************************************************)

semgrep-core/src/core/Rule.ml
@@ -121,7 +121,7 @@ and metavar_cond =
    * the "regexpizer" optimizer (see Analyze_rule.ml).
    *)
   | CondRegexp of MV.mvar * regexp
-  | CondPattern of MV.mvar * formula
+  | CondPattern of MV.mvar * Lang.t option * formula
 [@@deriving show, eq]
 
 (*****************************************************************************)
@@ -149,7 +149,8 @@ type formula_old =
 (* extra conditions, usually on metavariable content *)
 and extra =
   | MetavarRegexp of MV.mvar * regexp
-  | MetavarPattern of MV.mvar * formula
+  (* TODO: Generalize `Lang.t option` to `xlang`. *)
+  | MetavarPattern of MV.mvar * Lang.t option * formula
   | MetavarComparison of metavariable_comparison
   | PatWherePython of string
 
@@ -241,7 +242,8 @@ let rewrite_metavar_comparison_strip mvar cond =
 let convert_extra x =
   match x with
   | MetavarRegexp (mvar, re) -> CondRegexp (mvar, re)
-  | MetavarPattern (mvar, formula) -> CondPattern (mvar, formula)
+  | MetavarPattern (mvar, opt_lang, formula) ->
+      CondPattern (mvar, opt_lang, formula)
   | MetavarComparison comp -> (
       match comp with
       (* do we care about strip and base? should not Eval_generic handle it?

semgrep-core/src/parsing/Parse_rule.ml
@@ -318,15 +318,31 @@ and parse_extra env x =
   | "metavariable-pattern", J.Object xs -> (
       match find_fields [ "metavariable" ] xs with
       | [ ("metavariable", Some (J.String metavar)) ], rest ->
+          let id, _ = env in
+          let env', opt_lang, rest' =
+            match find_fields [ "language" ] rest with
+            | [ ("language", Some (J.String s)) ], rest' ->
+                let lang =
+                  match Lang.lang_of_string_opt s with
+                  | None ->
+                      raise
+                        (E.InvalidLanguageException
+                           (id, spf "unsupported language: %s" s))
+                  | Some l -> l
+                in
+                let env' = (id, R.L (lang, [])) in
+                (env', Some lang, rest')
+            | ___else___ -> (env, None, rest)
+          in
           let pformula =
-            match rest with
-            | [ x ] -> parse_formula env x
+            match rest' with
+            | [ x ] -> parse_formula env' x
             | x ->
                 pr2_gen x;
                 error "wrong rule fields"
           in
           let formula = R.formula_of_pformula pformula in
-          R.MetavarPattern (metavar, formula)
+          R.MetavarPattern (metavar, opt_lang, formula)
       | x ->
           pr2_gen x;
           error "metavariable-pattern:  wrong parse_extra fields" )

semgrep-core/tests/OTHER/rules/metavar_pattern_lang.py
@@ -1,0 +1,4 @@
+#ruleid:test-mvp-lang
+eval_C("x++;")
+#OK:test-mvp-lang
+eval_C("++x;")

semgrep-core/tests/OTHER/rules/metavar_pattern_lang.yaml
@@ -1,0 +1,15 @@
+rules:
+  - id: test-mvp-lang
+    languages:
+      - python
+    message: Working!
+    patterns:
+      - pattern: |
+          eval_C("$CODE")
+      - metavariable-pattern:
+          metavariable: $CODE
+          language: C
+          patterns:
+              - pattern: |
+                  $X++;
+    severity: WARNING

semgrep-core/tests/OTHER/rules/metavar_pattern_lang1.py
@@ -1,0 +1,4 @@
+#OK:test-mvp-lang
+eval_C("x++;")
+#ruleid:test-mvp-lang
+eval_C("++x;")

semgrep-core/tests/OTHER/rules/metavar_pattern_lang1.yaml
@@ -1,0 +1,16 @@
+rules:
+  - id: test-mvp-lang
+    languages:
+      - python
+    message: Working!
+    patterns:
+      - pattern: |
+          eval_C("$CODE")
+      - metavariable-pattern:
+          metavariable: $CODE
+          language: C
+          patterns:
+              - pattern: ...
+              - pattern-not: |
+                  $X++;
+    severity: WARNING

commit d328e96d63defc4fee0a07abcfd3fcdca8ce109e

metavariable-pattern: Fix nested language implementation
1. Pass the right xlang to `matches_of_pattern`.
2. Adjust parse info after `with_tmp_file`.

Fixes: 70a20605ae4 ("metavariable-pattern: Allow matching content under another language (#3366)")

test plan:
make test # tests included

semgrep-core/src/engine/Match_rules.ml
@@ -227,9 +227,9 @@ let lazy_force x = Lazy.force x [@@profiling]
 (* Adapters *)
 (*****************************************************************************)
 
-let (mini_rule_of_pattern : R.t -> Pattern.t * Rule.pattern_id * string -> MR.t)
-    =
- fun r (pattern, id, pstr) ->
+let (mini_rule_of_pattern :
+      R.xlang -> Pattern.t * Rule.pattern_id * string -> MR.t) =
+ fun xlang (pattern, id, pstr) ->
   {
     MR.id = string_of_int id;
     pattern;
@@ -239,7 +239,7 @@ let (mini_rule_of_pattern : R.t -> Pattern.t * Rule.pattern_id * string -> MR.t)
     message = "";
     severity = MR.Error;
     languages =
-      ( match r.R.languages with
+      ( match xlang with
       | R.L (x, xs) -> x :: xs
       | R.LNone | R.LGeneric -> raise Impossible );
     (* useful for debugging timeout *)
@@ -866,17 +866,19 @@ and match_range_with_pattern env r mvar opt_lang formula =
   | Some mval -> (
       let mval_range = MV.range_of_mvalue mval in
       match (opt_lang, mval) with
-      | Some lang, MV.Text (content, _)
-      | Some lang, MV.E (G.L (G.String (content, _))) ->
+      | Some lang, MV.Text (content, tok)
+      | Some lang, MV.E (G.L (G.String (content, tok))) ->
           (* The matched text will be interpreted according to `lang'. *)
           let lazy_ast_and_errors =
-            lazy_ast_of_string lang content (fun () ->
+            lazy_ast_of_string lang tok content (fun () ->
                 spf
                   "rule %s: metavariable-pattern: failed to fully parse the \
                    content of %s"
                   env.rule.id mvar)
           in
-          eval_nested_formula env env.xlang formula lazy_ast_and_errors
+          eval_nested_formula env
+            (R.L (lang, []))
+            formula lazy_ast_and_errors
             (lazy content)
             None
       | Some _lang, _mval ->
@@ -902,7 +904,19 @@ and match_range_with_pattern env r mvar opt_lang formula =
                 lazy_content
                 (Some { r with r = mval_range }) ) )
 
-and lazy_ast_of_string lang str warn_msg =
+and lazy_ast_of_string lang base_tok str warn_msg =
+  let fix_toks ast =
+    let base_loc = Parse_info.token_location_of_info base_tok in
+    let visitor =
+      Map_AST.mk_visitor
+        {
+          Map_AST.default_visitor with
+          Map_AST.kinfo =
+            (fun (_, _) tok -> Parse_info.adjust_info_wrt_base base_loc tok);
+        }
+    in
+    visitor.Map_AST.vprogram ast
+  in
   lazy
     (let ext =
        match Lang.ext_of_lang lang with x :: _ -> x | [] -> assert false
@@ -911,6 +925,7 @@ and lazy_ast_of_string lang str warn_msg =
          let { Parse_target.ast; errors; _ } =
            Parse_target.parse_and_resolve_name_use_pfff_or_treesitter lang file
          in
+         let ast = fix_toks ast in
          if errors <> [] then pr2 (warn_msg ());
          (ast, errors)))
 
@@ -920,7 +935,7 @@ and eval_nested_formula env xlang formula lazy_ast_and_errors lazy_content
      * we could maybe factorize things *)
   let xpatterns = xpatterns_in_formula formula in
   let res =
-    matches_of_xpatterns env.config env.rule env.equivalences
+    matches_of_xpatterns env.config xlang env.equivalences
       (env.file, xlang, lazy_ast_and_errors, lazy_content)
       xpatterns
   in
@@ -965,7 +980,7 @@ let check hook default_config rules equivs file_and_more =
                let xpatterns = xpatterns_in_formula formula in
                let config = r.settings ||| default_config in
                let res =
-                 matches_of_xpatterns config r equivs
+                 matches_of_xpatterns config r.languages equivs
                    (file, xlang, lazy_ast_and_errors, lazy_content)
                    xpatterns
                in

semgrep-core/tests/OTHER/rules/metavar_pattern_lang2.generic
@@ -1,0 +1,11 @@
+<!-- ruleid:test-mvp-lang -->
+<script>
+if A and B:
+    foo()
+</script>
+
+<!-- OK:test-mvp-lang -->
+<script>
+if A and B:
+    bar()
+</script>

semgrep-core/tests/OTHER/rules/metavar_pattern_lang2.yaml
@@ -1,0 +1,16 @@
+rules:
+  - id: test-mvp-lang
+    languages:
+      - generic
+    message: Working!
+    patterns:
+      - pattern: |
+          <script>$...CODE</script>
+      - metavariable-pattern:
+          metavariable: $...CODE
+          language: python
+          patterns:
+              - pattern: |
+                  if A and $X:
+                      foo(...)
+    severity: WARNING